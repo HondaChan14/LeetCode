f3c11b40a2368151fa61b20a8debcadc780d8418
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-07-02 09:30:10 -0400

    Code Challenge 7-2-22

---
+++ CC 2022-07-02/Pailindrone.js
@@ -0,0 +1,60 @@
+/*
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+
+Solution:
+/**
+ * @param {number} x
+ * @return {boolean}
+ 
+
+// x = integer return true if palindrome
+// 121 is a palindrome 
+// while 123 is not.
+
+var isPalindrome = function(x) {
+    // Base condition
+   if (x < 0) {
+       return false;
+   }
+   // Store the number in a variable
+   let number = x;
+   // This will store the reverse of the number
+   let reverse = 0;
+   while (number > 0) {
+       reverse = reverse * 10 + number % 10;
+       number = parseInt(number / 10);
+   }
+   return x === reverse;
+   
+};
+
+*/
